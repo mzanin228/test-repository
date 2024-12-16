2e5825afefc002a5dbc01ab5ebcec6043d2e9ae6
dick <dick>
2024-12-16 13:51:03 +0300

    New branch feat-1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект ФИО";
+  const greeting = "Занин Михаил Алексеевич";
 
   console.log('This is App ');
   return (
